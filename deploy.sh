da4aaeb58c5b3fc95bfde58adac6b9bcb8c2b658
caicailiushui <15757826110@163.com>
2020-05-09 11:08:35 +0800

    update: moment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/zsh
 /usr/local/bin/hugo
-#git add --all
-#git commit -a -m "deploy from Alfred"
-#git push -u origin master
-#echo "Site is deployed successfully"
+git add --all
+git commit -a -m "deploy from Alfred"
+git push -u origin master
+echo "Site is deployed successfully"
